92c93ad195b3f6563046e537fa5651ba76a238e3
Sviat6239 <sviatoslav6239@gmail.com>
2026-02-22 19:45:26 +0100

    added lang switch

--- static/js/index.js
+++ static/js/index.js
@@ -1,37 +1,41 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const btn = document.getElementById('lang-switch');
-    const flag = document.getElementById('lang-flag');
 
-    const POLAND_FLAG = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Flag_of_Poland.svg/640px-Flag_of_Poland.svg.png";
-    const UKRAINE_FLAG = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Flag_of_Ukraine.svg/250px-Flag_of_Ukraine.svg.png";
-    const rus_flag = ""
+    const toggleBtn = document.getElementById('lang-toggle');
+    const menu = document.getElementById('lang-menu');
+    const menuItems = document.querySelectorAll('[data-set-lang]');
+    const translatable = document.querySelectorAll('[data-lang]');
 
     let currentLang = document.documentElement.lang || 'pl';
 
-    const setFlagFor = (langToSwitchTo) => {
-        if (langToSwitchTo === 'ua') {
-            flag.src = UKRAINE_FLAG;
-            flag.alt = "Українська";
-        } else if(langToSwitchTo === 'pl'){
-            flag.src = POLAND_FLAG;
-            flag.alt = "Polski";
-        } else{
-            flag.src = rus_flag;
-            flag.alt = "русский";
-        }
-    };
-
-    setFlagFor(currentLang === 'pl' ? 'ua'  ? 'ru': 'pl');
-
-    btn.addEventListener('click', () => {
-        const newLang = currentLang === 'pl' ? 'ua' ? 'ru' : 'pl';
-
-        document.querySelectorAll('[data-lang]').forEach(el => {
-            const text = el.getAttribute(`data-lang-${newLang}`);
-            if (text !== null) el.textContent = text;
+    function applyLanguage(lang) {
+        translatable.forEach(el => {
+            const text = el.getAttribute(`data-lang-${lang}`);
+            if (text) el.textContent = text;
         });
-        setFlagFor(currentLang);
-        document.documentElement.lang = newLang;
-        currentLang = newLang;
+
+        document.documentElement.lang = lang;
+        toggleBtn.textContent = lang.toUpperCase();
+        currentLang = lang;
+    }
+
+    toggleBtn.addEventListener('click', () => {
+        menu.classList.toggle('hidden');
     });
+
+
+    menuItems.forEach(item => {
+        item.addEventListener('click', () => {
+            const selectedLang = item.getAttribute('data-set-lang');
+            applyLanguage(selectedLang);
+            menu.classList.add('hidden');
+        });
+    });
+
+    document.addEventListener('click', (e) => {
+        if (!e.target.closest('.lang-wrapper')) {
+            menu.classList.add('hidden');
+        }
+    });
+
+    applyLanguage(currentLang);
 });
